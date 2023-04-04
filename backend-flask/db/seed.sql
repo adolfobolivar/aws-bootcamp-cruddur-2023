9bba095c83d9e9a47b7a1406e73c8b1ab6e6ce83
adolfobolivar <isos83@hotmail.com>
2023-04-04 15:26:37 +0000

    update ddb schema

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('adolfo bolivar', 'isos83' , 'isos83@hotmail.com', 'MOCK');
   ('mariu bolivar', 'mariu028' , 'mariu028@gmail.com', 'MOCK');
-  ('Londo Mollari', 'londo' , 'lmollari@centari.com',  'MOCK');
+ --  ('Londo Mollari', 'londo' , 'lmollari@centari.com',  'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
